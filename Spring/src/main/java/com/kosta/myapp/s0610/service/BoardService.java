8613927da8ac232d99289b886ff92a18241037dd
salbi <salbi0925@gmail.com>
2017-06-10 20:24:49 +0900

    책 mybatis 연결까지 실행 동작관계 모름

--- Spring/src/main/java/com/kosta/myapp/s0610/service/BoardService.java
+++
@@ -1,9 +0,0 @@
-package com.kosta.myapp.s0610.service;
-
-import java.util.List;
-
-import com.kosta.myapp.s0610.vo.BoardinfoVO;
-
-public interface BoardService {
-	List<BoardinfoVO> s_ALLBoardList() throws Exception;
-}
