cbb837857f04d24d87ea6870617001332c9ec061
salbi <salbi0925@gmail.com>
2017-06-12 11:28:26 +0900

    137

---
+++ Spring/src/main/java/org/zerock/presistence/MemberDAO.java
@@ -0,0 +1,9 @@
+package org.zerock.presistence;
+
+import org.zerock.domain.MemberVO;
+
+public interface MemberDAO {
+	public String getTime();
+
+	public void insertMember(MemberVO vo);
+}
---
+++ Spring/src/main/java/org/zerock/presistence/MemberDAOImpl.java
@@ -0,0 +1,27 @@
+package org.zerock.presistence;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+import org.zerock.domain.MemberVO;
+
+@Repository //DAO라고 설정
+public class MemberDAOImpl implements MemberDAO {
+
+	@Inject
+	private SqlSession sqlSession;
+	
+	private static final String namespace = "org.zerock.mapper.MemberMapper";
+	
+	@Override
+	public String getTime() {
+		return sqlSession.selectOne(namespace+".getTime");
+	}
+
+	@Override
+	public void insertMember(MemberVO vo) {
+		sqlSession.insert(namespace+".insertMember",vo);
+	}
+
+}
