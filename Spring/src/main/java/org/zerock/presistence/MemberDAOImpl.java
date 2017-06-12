523e34693953fdd7ab2f2d4e7afad2c54cb47d23
salbi <salbi0925@gmail.com>
2017-06-12 12:28:27 +0900

    1

--- Spring/src/main/java/org/zerock/presistence/MemberDAOImpl.java
+++ Spring/src/main/java/org/zerock/presistence/MemberDAOImpl.java
@@ -1,27 +1,45 @@
 package org.zerock.presistence;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 import org.zerock.domain.MemberVO;
 
-@Repository //DAO라고 설정
+@Repository // DAO라고 설정
 public class MemberDAOImpl implements MemberDAO {
 
 	@Inject
 	private SqlSession sqlSession;
-	
+
 	private static final String namespace = "org.zerock.mapper.MemberMapper";
-	
+
 	@Override
 	public String getTime() {
-		return sqlSession.selectOne(namespace+".getTime");
+		return sqlSession.selectOne(namespace + ".getTime");
 	}
 
 	@Override
 	public void insertMember(MemberVO vo) {
-		sqlSession.insert(namespace+".insertMember",vo);
+		sqlSession.insert(namespace + ".insertMember", vo);
+	}
+
+	@Override
+	public MemberVO readMember(String userid) throws Exception {
+		return (MemberVO) sqlSession.selectOne(namespace + ".selectMember", userid);
+	}
+
+	@Override
+	public MemberVO readWithPW(String userid, String userpw) throws Exception {
+		Map<String, Object> paramMap = new HashMap<>();
+
+		paramMap.put("userid", userid);
+		paramMap.put("userpw", userpw);
+
+		return sqlSession.selectOne(namespace + ".readWithPW", paramMap);
 	}
 
 }
